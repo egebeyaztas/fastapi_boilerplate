43f569bd5326a34bfbf4190089f07bebaf38a9d0
egebeyaztas <egebeyaztas0@gmail.com>
2024-10-06 15:31:23 +0300

    second commit

--- src/main.py
+++ src/main.py
@@ -2,8 +2,13 @@ from fastapi import FastAPI, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlmodel import SQLModel
 from contextlib import asynccontextmanager
-
+from src.auth.routers import auth_router
 from src.database import engine, get_session
+from src.exceptions import register_all_errors
+
+version = "v1"
+
+version_prefix =f"/api/{version}"
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -16,6 +21,9 @@ async def lifespan(app: FastAPI):
     print("Server is shutting down...")
 
 app = FastAPI(lifespan=lifespan)
+app.include_router(auth_router, prefix=f"{version_prefix}/auth", tags=["auth"])
+
+register_all_errors(app)
 
 @app.get("/")
 async def root(session: AsyncSession = Depends(get_session)):
